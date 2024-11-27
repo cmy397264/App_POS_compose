ed68736dba2cbab78181c571df6842e0dfddd04c
Hw0o0c <Hw0o0c@github.com>
2024-11-27 15:45:33 +0900

    -TableScreen, OrderScreen 오류해결,
    -PayScreen 작업 시작

--- app/src/main/java/com/example/app_pos_compose/ui/OrderScreen.kt
+++ app/src/main/java/com/example/app_pos_compose/ui/OrderScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -35,7 +34,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
